82c12d88d569bf9a24745a41091c9bf2b45e3081
asa aveanna <github@holomaplefeline.net>
2019-07-05 19:32:17 -0700

    Expose the old method for testing.

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const app = require('./app');
+const http = require('http');
 const https = require('https');
+
+http.createServer(app).listen(80);
+
+console.log('Running on http://localhost:80');
 
 https.createServer(app).listen(443);
 
